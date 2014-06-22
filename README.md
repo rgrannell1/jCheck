a92a7d30b639dc29e495f7554b8f782b45611385
rgrannell1 <r.grannell2@gmail.com>
2014-06-22 15:49:50 +0100

    adding JSDocs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 jCheck
 ======
+
+jCheck is a minimal property-based testing framework for JavaScript.
+Its main focus is to remove testing boilerplate; you don't need to
+specify test-cases or test-case generators for jCheck tests.
+
+# Installation.
+
--- test/test-arithmetic.js
+++ test/test-arithmetic.js
@@ -1,11 +1,14 @@
 
 over_('a', 'b') .
 
-describe('addition is commutative') .
+describe('addition & multiplication are commutative') .
 when_(
 	typeof(a) === 'number' && a === a &&
 	typeof(b) === 'number' && b === b,
-	a + b === b + a
+	a + b === b + a,
+	a * b === b * a
 ) .
+
+describe('multiplication is commutative') .
 
 run(1)
