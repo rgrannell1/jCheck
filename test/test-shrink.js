2fc7a77f89cd02c420dbb91dbcfbf4c87b188ac8
rgrannell1 <r.grannell2@gmail.com>
2014-07-01 21:49:38 +0100

    started testing shrink methods, found the NaN-causing bug.

---
+++ test/test-shrink.js
@@ -0,0 +1,42 @@
+
+_remove     = require('../lib/shrink')._remove
+_substitute = require('../lib/shrink')._substitute
+_score      = require('../lib/shrink')._remove
+
+remove     = require('../lib/shrink').remove
+substitute = require('../lib/shrink').substitute
+score      = require('../lib/shrink').remove
+
+_removeCategory     = require('../lib/shrink')._removeCategory
+_scoreCategory      = require('../lib/shrink')._scoreCategory
+_substituteCategory = require('../lib/shrink')._substituteCategory
+
+
+
+
+
+
+const assert = require('assert')
+const is     = require('is')
+
+console.log('_remove.array')
+
+	assert(_remove.array([]).length === 0)
+	assert(is.array( _remove.array([]) ))
+	assert(is.array( _remove.array([1]) ))
+	assert(is.array( _remove.array([1, 2]) ))
+	assert(_remove.array([1, 2]).length === 1)
+
+console.log('_remove.string')
+
+	assert(_remove.string('a') === '')
+	assert(_remove.string('ab').length === 1)
+	assert(is.string(_remove.string('ab')))
+
+console.log('_remove.number')
+
+
+
+console.log('remove')
+
+	console.log(  remove(100) < 100   )
