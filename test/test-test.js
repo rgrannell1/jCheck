1ffc85f64d24a717d5d046ac5c878be72d04fea6
rgrannell1 <r.grannell2@gmail.com>
2014-07-29 00:36:26 +0100

    removed a test designed to fail.

--- test/test-test.js
+++ test/test-test.js
@@ -9,23 +9,6 @@ const holdsWhen  = jCheck.holdsWhen
 const holdsWhen_ = jCheck.holdsWhen_
 const run        = jCheck.run
 
-
-var isa = function (type, val) {
-	return toString.call(val) === '[object ' + type + ']'
-}
-
-over_("num")
-
-.describe("test that a number double-equals itself.")
-.holdsWhen_(
-	function (num) {return isa("Number", num)},
-	function (num) {return num == num}
-)
-
-.run()
-
-
-
 over_('val')
 
 .describe('this is a work')
